f0342cb91e053832ce4911657adbdc4f724d3353
nemopss <alexey_gladilin@mail.ru>
2024-04-27 03:52:11 +0300

    Implemented quicksort

---
+++ src/main/java/com/nemopss/Quicksort/Qsort.java
@@ -0,0 +1,30 @@
+package com.nemopss.Quicksort;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Qsort {
+
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(5, 3, 27, 66, 0, 6, 7, 9, 17);
+        System.out.println(qsort(arr));
+    }
+
+    public static List<Integer> qsort(List<Integer> list) {
+        if (list.size() < 2) {
+            return list;
+        }
+        Integer pivot = list.get(0);
+        List<Integer> left = list.stream().skip(1).filter(el -> el <= pivot).toList();
+        List<Integer> right = list.stream().skip(1).filter(el -> el > pivot).toList();
+
+        return Stream.of(
+                qsort(left).stream(),
+                Stream.of(pivot),
+                qsort(right).stream())
+        .flatMap(Function.identity()).toList();
+
+    }
+}
